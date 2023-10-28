0c5d52c59bc3a637c467492a6d144f5f34d1d5b1
Dmitriy <D.Krimin@gmail.com>
2023-10-28 23:58:55 +0300

    3

---
+++ lab3/lab3c.js
@@ -0,0 +1,6 @@
+let a = 13; 
+let b = 1; 
+for(i =1; i < a +1; ++i) {
+    b = b * i
+} 
+console.log(b); // выводим факториал
---
+++ lab3/lab3r.js
@@ -0,0 +1,2 @@
+function fak(...x) { return [...x].reduce((x, y) => x * y); } 
+console.log(fak(1, 2, 3, 4 )); 
