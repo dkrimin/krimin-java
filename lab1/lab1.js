e82fe3d0891ced9ef1885ea0a376c7285ac36c3a
Dmitriy <D.Krimin@gmail.com>
2023-10-28 22:48:00 +0300

    1

---
+++ lab1/lab1.js
@@ -0,0 +1,13 @@
+const [n1, n2, n3, n4, n5, n6] = [2, 3, 18, 4, -5, -8];
+let d = n1*n5-n2*n4;
+//console.log(d);
+let d2 = n3*n5-n2*n6;
+//console.log(d2);
+let d3 = n1*n6-n3*n4;
+//console.log(d3);
+let x = d2/d;
+console.log("x= " + String(x));
+let y = d3/d;
+console.log("y= " + String(y));
+
+
